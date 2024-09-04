97bbbcaa9d12458d43cbc7f44685f258a1307345
Chanpreet Singh <chanpreet3000@gmail.com>
2024-09-05 00:15:47 +0530

    Changed headers

--- utils.js
+++ utils.js
@@ -1,7 +1,7 @@
 export const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 export const headers = {
-  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/109.0',
-  'Accept': 'application/xml,application/xhtml+xml,text/html,text/xml',
+  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36',
+  'Accept': 'application/json, text/plain, */*',
   'Accept-Language': 'en-US,en;q=0.9',
 };
